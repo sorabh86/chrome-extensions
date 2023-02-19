880426878331583f060fa692772ca3768f9a8920
sorabh86 <ssorabh.ssharma@gmail.com>
2023-02-20 00:14:30 +0530

    export library module

--- prodprac001/webpack.config.js
+++ prodprac001/webpack.config.js
@@ -1,11 +1,27 @@
 const path = require('path');
 
+/**
+ * Webpack placeholders
+ * [hash] | [chunkhash] | [name] | [id] | [query] | [contenthash]
+ */
+/**
+ * Webpack library target options
+ * var | amd | commonjs | umd (Universal Module Defination)
+ */
+
 module.exports = {
 	mode:"development", // default "production" or "development"
-	entry:"./src/index.js", // default entry for javascript | string, object or array path to files
+	entry:{// default entry for javascript | string, object or array path to files
+		app:"./src/index.js", // entry placeholder
+	}, 
 	output: { // output configuration
 		path: path.resolve(__dirname, 'build'), // folder path to output
 		// filename: '../build/build.js', // output to filename create new folder outside
-		filename: 'js/build.js', // output filename with folder
+		filename: 'js/[name].build.js', // output filename with folder with webpack placeholders
+		publicPath: '/assets/', // path that webpack used to internally(path on server) while in development of application
+		libraryTarget:'umd', // define various export syntax implementations like CommonJS, AMD define, ES6 import  
+		library:'sosLib', // it is library name and required if you set libraryTarget, this will be available on global scope
+
+
 	}
 }
