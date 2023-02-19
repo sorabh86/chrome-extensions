e5865ec1c39a7c250389bb8ef74202508532cf9c
sorabh86 <ssorabh.ssharma@gmail.com>
2023-02-19 20:37:16 +0530

    configure webpack output folder

--- prodprac001/webpack.config.js
+++ prodprac001/webpack.config.js
@@ -1,3 +1,11 @@
+const path = require('path');
+
 module.exports = {
-	mode:"development" // default "production" or "development"
+	mode:"development", // default "production" or "development"
+	entry:"./src/index.js", // default entry for javascript | string, object or array path to files
+	output: { // output configuration
+		path: path.resolve(__dirname, 'build'), // folder path to output
+		// filename: '../build/build.js', // output to filename create new folder outside
+		filename: 'js/build.js', // output filename with folder
+	}
 }
